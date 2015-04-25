769c38b4f82ec72c149e3ad0e0155719bfc7f2e8
mortalis13 <stangerroman@gmail.com>
2015-04-25 09:47:11 +0300

    Improve read JSON performance

--- src/TreeViewer.java
+++ src/TreeViewer.java
@@ -1,9 +1,13 @@
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.List;
@@ -49,38 +53,41 @@ public class TreeViewer{
       ArrayList<TreeNode> tree;
       String json,file;
       
-//      file="en";
+      file="en";
       file="light-test";
+      file="light-test-formatted";
       file="3-programming";
+//      file="3-programming-formatted";
       
+      long time1,time2;
+      time1=System.currentTimeMillis();
       json=readJSON(file);
+      time2=System.currentTimeMillis();
+      System.out.println("Read time: "+formatTime((int) (time2-time1), "%.2f s") );
+      
+      time1=System.currentTimeMillis();
       tree=decodeJSON(json);
+      time2=System.currentTimeMillis();
+      System.out.println("Decode time: "+formatTime((int) (time2-time1), "%.2f s") );
+      
+//      json=readJSON(file);
+//      tree=decodeJSON(json);
       
       return tree;
     }
     
     public String readJSON(String file){
-      String doc = "", path, line;
-      BufferedReader br = null;
-      int c;
-
+      String doc = "", path; 
       path="json/"+file+".json";
       
       try {
-        br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
-        while ((line = br.readLine()) != null) {
-          doc += line+"\n";
-        }
+        Path filePath = Paths.get(path);
+        Charset charset = Charset.forName("UTF-8");
+        
+        byte[] data = Files.readAllBytes(filePath);
+        doc=new String(data, charset);
       } catch (IOException e) {
         e.printStackTrace();
-      } finally {
-        if (br != null){
-          try {
-              br.close();
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        }
       }
       
       return doc;
@@ -142,6 +149,7 @@ public class TreeViewer{
       return timeFormat.toString();
     }
     
+    // time measure tmeplate
     private void showProcessTime(){
       long time1,time2;
       time1=System.currentTimeMillis();
@@ -150,15 +158,15 @@ public class TreeViewer{
       System.out.println("Read time: "+formatTime((int) (time2-time1), "%.2f s") );
     }
   }
+  
+  public TreeViewer(){
+    window=TreeViewerFrame.window;
+  }
     
   public void showTree(){
     TreeWorker worker=new TreeWorker();
     worker.addPropertyChangeListener(worker);
     worker.execute();
-  }
-  
-  public TreeViewer(){
-    window=TreeViewerFrame.window;
   }
 
 }
